3659c156d143d132517334629a6386a1eb62019e
MoonLord-LM <me@moonlord.cn>
2024-05-03 13:50:23 +0800

    Update MoonAssistantApplication.java

--- src/main/java/cn/moonlord/ai/MoonAssistantApplication.java
+++ src/main/java/cn/moonlord/ai/MoonAssistantApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAsync
+@EnableAsync(proxyTargetClass = true)
 @EnableScheduling
 @SpringBootApplication
 public class MoonAssistantApplication {
