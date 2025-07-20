1964da7876f94c2378539e02e13bd718e41bbf1b
MoonLord-LM <me@moonlord.cn>
2025-07-20 11:53:05 +0800

    Update

--- TempFileStorage/src/main/java/cn/moonlord/tempfilestorage/service/VideoScanService.java
+++ TempFileStorage/src/main/java/cn/moonlord/tempfilestorage/service/VideoScanService.java
@@ -2,7 +2,6 @@ package cn.moonlord.tempfilestorage.service;
 
 import cn.moonlord.tempfilestorage.vo.LogicalVolumeVO;
 import cn.moonlord.tempfilestorage.vo.VideoFileVO;
-import jakarta.annotation.PostConstruct;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
